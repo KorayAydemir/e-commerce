f2e7612343ba4792d4ceefd4a06e2db716829c4f
Koray Aydemir <korayaydemir276@gmail.com>
2023-06-04 20:17:56 +0300

    feat: can fetch from supabase postgresql db

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
 import { Products } from "@components/components/home/Products";
 import WideLayout from "@components/components/layouts/layout_wide";
 import { Header } from "@components/components/shared/Header";
+import { supabase } from "../../lib/supabaseClient";
 
-export default function Home({ productsData }: any) {
+export default function Home({ productsData, products }: any) {
+    console.log(products);
     return (
         <>
             <Header />
@@ -18,9 +20,11 @@ export default function Home({ productsData }: any) {
 export const getStaticProps = async () => {
     const response = await fetch("https://dummyjson.com/products");
     const newData = await response.json();
+    const { data } = await supabase.from("categories").select("*");
     return {
         props: {
             productsData: newData,
+            products: data,
         },
     };
 };
