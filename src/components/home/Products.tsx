ba61d996fcbee16baca948d1b27eda9781cdfd5d
Koray Aydemir <korayaydemir276@gmail.com>
2023-06-04 14:20:32 +0300

    feat: add filter button to toggle sidenav

--- src/components/home/Products.tsx
+++ src/components/home/Products.tsx
@@ -1,7 +1,12 @@
 import Image from "next/image";
+import { IoFilterSharp } from "react-icons/io5";
+import { RiArrowDownSLine, RiArrowUpSLine } from "react-icons/ri";
 import { SingularProduct } from "./SingularProduct";
+import { useState } from "react";
 
 export const Products = ({ productsData }: any) => {
+    const [filterState, setFilterState] = useState("hidden");
+
     const uniqueProductCategories = new Set<string>();
     productsData?.products.forEach((product: any) => {
         uniqueProductCategories.add(product.category);
@@ -24,7 +29,7 @@ export const Products = ({ productsData }: any) => {
 
     const productsCategories = procateg.map((cat: string) => {
         return (
-            <div key={cat} className="text-white">
+            <div key={cat} className="font-bold">
                 {cat}
             </div>
         );
@@ -35,21 +40,44 @@ export const Products = ({ productsData }: any) => {
     });
 
     const SideBar = (
-        <div className="flex flex-col w-1/6 ml-auto px-2">
-            <aside className="bg-slate-600 py-5">
-                <div className="max-h-80 overflow-y-scroll">
-                    {productsCategories}
+        <aside
+            className={`flex flex-col w-1/6 ml-auto px-2 text-white lg:block ${filterState}`}
+        >
+            <div className="max-h-80 overflow-y-scroll leading-loose">
+                {productsCategories}
+            </div>
+            <div className="before:absolute before:-top-3 before:left-0 before:w-full before:h-[1px] before:bg-white relative mt-12 w-11/12">
+                <div className="flex justify-between">
+                    <h3 className="font-bold text-lg m-0">Gender</h3>
+                    <RiArrowUpSLine className="my-auto text-xl" />
                 </div>
-            </aside>
-        </div>
+            </div>
+        </aside>
     );
+
+    const filtersHandler = () => {
+        setFilterState((prev) => (prev === "hidden" ? "block" : "hidden"));
+    };
 
     return (
         <div className="flex flex-col">
-            <div className="py-4  px-2">
-                <h2 className="font-bold text-white text-xl mb-2 block">
+            <div className="py-4 px-2 flex flex-row justify-between items-center">
+                <h2 className="font-bold text-white text-xl mb-2 inline">
                     New (500)
                 </h2>
+                <div className="flex">
+                    <div
+                        className="flex flex-row inline mr-2"
+                        onClick={filtersHandler}
+                    >
+                        <span className="text-white mr-1">Filters</span>
+                        <IoFilterSharp className="text-white text-xl my-auto" />
+                    </div>
+                    <div className="flex flex-row inline">
+                        <span className="text-white mr-1">Sort By</span>
+                        <RiArrowDownSLine className="text-white text-xl my-auto" />
+                    </div>
+                </div>
             </div>
             <div className="flex flex-row">
                 {SideBar}
